d545fe20bf69b57fe7a220236eb16b13e3120707
PedroDeAlbuquerque <pedro.de.albuquerque@hotmail.com>
2017-02-09 17:17:39 -0200

    company_user_reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209182756) do
+ActiveRecord::Schema.define(version: 20170209191007) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20170209182756) do
     t.string   "operating_hours"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
   create_table "newslleters", force: :cascade do |t|
